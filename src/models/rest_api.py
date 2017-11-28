d41253ff96d5f8192185f8632c18218b97564849
Patric Steiner <patric.steiner1@gmail.com>
2017-11-28 21:18:06 +0000

    added functionality to add new workpiece images

--- src/models/rest_api.py
+++ src/models/rest_api.py
@@ -8,6 +8,9 @@ import tensorflow as tf
 import predict_pb2
 import prediction_service_pb2
 
+from random import *
+import os
+
 tf.app.flags.DEFINE_string("host", "127.0.0.1", "gRPC server host")
 tf.app.flags.DEFINE_integer("port", 9000, "gRPC server port")
 tf.app.flags.DEFINE_string("model_name", "ip5wke", "TensorFlow model name")
@@ -44,6 +47,8 @@ class Inference:
 
     def _route(self):
         self._app.route('/', method="POST", callback=self._POST)
+        self._app.route('/new_workpiece_id', method="GET", callback=self.new_workpiece_id)
+        self._app.route('/add_workpiece_image', method="POST", callback=self.add_workpiece_image)
 
     def start(self):
         self._app.run(host=self._host, port=self._port)
@@ -69,6 +74,33 @@ class Inference:
                              answer.outputs['classes'].string_val[2].decode("utf-8")]}
 
         return {"success": True, "result": result}
+
+    def new_workpiece_id(self):
+        # REST endpoint for getting a new workpiece id
+	print("new workpiece id requested")
+	new_id = randint(1000, 1000000) #TODO: get next free id instead
+	print("new workpiece id = " + str(new_id))
+
+        return {"workpieceId": new_id}
+
+    def add_workpiece_image(self):
+        # REST endpoint for adding an image for a new workpiece
+	print("adding image for new workpiece")
+	workpiece_id = bottle.request.json['workpieceId']
+	print(bottle.request)
+	print(bottle.request.json)
+	
+        image_number = bottle.request.json['imageNumber']
+	image = bottle.request.json['image']
+    	directory = 'new_workpieces/' + str(workpiece_id) + '/'
+    	if not os.path.exists(directory):
+	    os.makedirs(directory)
+   	f = open(directory + str(image_number) + '.jpg', 'wb')
+    	f.write(base64.b64decode(image))
+    	f.close()
+
+        return {"workpieceId": workpiece_id}
+
 
 
 if __name__ == '__main__':
